4a03ee458cfc894af12a06465767229b5a7e32c0
Gerolmed <werwolf420@yahoo.de>
2021-07-30 13:53:40 +0200

    Added resolvers

--- src/util/ModuleUtil.ts
+++ src/util/ModuleUtil.ts
@@ -2,6 +2,7 @@ import {ModuleJSON} from "../file-mappins/FileMappings";
 import * as fs from "fs";
 import * as Path from "path";
 import { type } from "os";
+import {REMOTE_MODULE_RESOLVERS} from "./remote/RemoteModuleResolver";
 
 export function isRemoteRepository(name: string): boolean {
     return name.split("/").length > 1;
@@ -16,7 +17,7 @@ export async function getModuleConfig(source: string, projectRoot: string, branc
     if(isRemoteRepository(source)) {
         return JSON.parse(await fs.promises.readFile(Path.join(projectRoot, "modules", source, "forkengine-module.json")))
     } else {
-
+        return REMOTE_MODULE_RESOLVERS.find(resolver => resolver.supports(source, branch || "main"))!.resolve(source, branch || "main")
     }
 }
 
